4d535dbd285520223520efc5ac8161123ab9f559
Josh Sanger <joshsanger88@gmail.com>
2015-06-20 10:56:39 -0400

    Adding Files

---
+++ documentation.php
@@ -0,0 +1,116 @@
+<?php
+	$active = 'documentation';
+	$title = 'Documentation';
+	require_once('./inc/header.php');
+?>
+	<section id="top-section">
+		<div class="container">
+			<h2>Moby.js Documentation</h2>
+			<p>The ins and outs of Moby.js</p>
+			<a href="<?php echo BASE_URL;?>/dist/moby1.0.zip">Download 1.0</a>
+		</div> <!-- /container -->
+	</section> <!-- / top-section -->
+	<article class="content">
+			<section class="separator">
+				<div class="container">
+					<h1>Description</h1>
+					<p>Moby.js easily converts a desktop <code class="language-markup">&lt;nav&gt;</code> into a mobile friendly version. When the mobile button is clicked, the mobile nav will animate in using one of the 7 preset animations. If these animations don't suit your fancy, you can simply add your own class to Moby's menu, and style it as you see fit. Nested <code class="language-markup">&lt;ul&gt;</code> elements (Drop down items) are hidden and can be expanded by the user, while still having the ability to click on the actual link. No more fake top level navigation links required!</p>
+				</div> <!-- /container -->
+			</section> <!-- /separator -->
+			<section class="separator">
+				<div class="container">
+					<h1>Available Options with defaults</h1>
+					<p>Below are the current options available with their defaults. Each options is explained further down the page.</p>
+<pre>
+<code class="language-javascript">
+// Moby.js will duplicate the #main-nav menu
+$('nav#main-nav').moby({
+	breakpoint			: 	960, 
+	closeButton		 	: 	true, 
+	closeButtonContent 	: 	'&lt;span&gt;X&lt;/span&gt; Close Menu',
+	enableEscape		: 	true, 
+	insertAfter			: 	'', 
+	insertBefore		: 	'',
+	menuClass		 	: 	'right-side', 
+	mobyTrigger		 	: 	'$("#moby-button")', 
+	overlay			 	: 	true, 
+	overlayClass 	 	: 	'dark', 
+	subMenuOpenIcon  	: 	'&lt;span&gt;&amp;#x25BC;&lt;/span&gt;', 
+	subMenuCloseIcon 	: 	'&lt;span&gt;&amp;#x25B2;&lt;/span&gt;',
+});</code>
+</pre>
+				</div> <!-- /container -->
+			</section>
+			<section>
+				<div class="container">
+					<h1>The Options</h1>
+					<div class="option">
+						<h2>breakpoint</h2>
+						<p>(<span class="dark-red-font">integer</span>) The number that is entered should match the breakpoint of when the targeted menu swicthes to mobile. If <code class="language-CSS">#moby</code> is open when the screen resizes above the set breakpoint, <code class="language-CSS">#moby</code> will close.</p>	
+						<p class="default">Default: 960</p>
+					</div> <!-- /wrap -->
+					<div class="option">
+						<h2>closeButton</h2>
+						<p>(<span class="blue-font">boolean</span>) Show or hide the close button at the top of <code class="language-CSS">#moby</code>. Set to <code class="language-javascript">false</code> to hide, <code class="language-javascript">true</code> to show.</p>	
+						<p class="default">Default: true</p>
+					</div> <!-- /wrap -->
+					<div class="option">
+						<h2>closeButtonContent</h2>
+						<p>(<span class="green-font">string</span>) You can specify the markup or text that you wish to display inside of the close button div. Target <code class="language-css">#moby-close</code> in your CSS to add your own style.</p>	
+						<p class="default">Default: '&lt;span&gt;X&lt;/span&gt; Close Menu'</p>
+					</div> <!-- /wrap -->
+					<div class="option">
+						<h2>enableEscape</h2>
+						<p>(<span class="blue-font">boolean</span>) Enable or disable closing <code class="language-CSS">#moby</code> with the escape key. Set to <code class="language-javascript">true</code> to enable, <code class="language-javascript">false</code> to disable.</p>	
+						<p class="default">Default: true</p>
+					</div> <!-- /wrap -->
+					<div class="option">
+						<h2>insertAfter</h2>
+						<p>(<span class="green-font">string</span>) You can specify markup or text that you wish to display below the mobile navigation. Target <code class="language-css">#moby-after</code> in your CSS to add your own style. If left blank, nothing will be inserted.</p>		
+						<p class="default">Default: ''</p>
+					</div> <!-- /wrap -->
+					<div class="option">
+						<h2>insertBefore</h2>
+						<p>(<span class="green-font">string</span>) You can specify markup or text that you wish to display above the mobile navigation (right underneath the close button). Target <code class="language-css">#moby-before</code> in your CSS to add your own style. If left blank, nothing will be inserted.</p>		
+						<p class="default">Default: ''</p>
+					</div> <!-- /wrap -->
+					<div class="option">
+						<h2>menuClass</h2>
+						<p>(<span class="green-font">string</span>) This is the class that will be added to <code class="language-CSS">#moby</code>. There are preset animation you can use, or create your own with css.</p>
+						<p>Available transitions are: 'left-side' | 'right-side' | 'top-full' | 'bottom-full' | 'shrink-in' | 'grow-out' | 'fade' | 'your-own-class'</p>		
+						<p class="default">Default: 'right-side'</p>
+					</div> <!-- /wrap -->
+					<div class="option">
+						<h2>mobyTrigger</h2>
+						<p>(<span class="green-font">string</span>) You MUST specify which which element will trigger <code class="language-CSS">#moby</code> to open. It is recommended to NOT use an <code class="language-markup">&lt;a&gt;</code> tag for the trigger, but instead use a <code class="language-markup">&lt;span&gt;</code> tag. If not set, <code class="language-markup">#moby-button</code> will be set to the default trigger.</p>		
+						<p class="default">Default: '$("#moby-button")'</p>
+					</div> <!-- /wrap -->
+					<div class="option">
+						<h2>overlay</h2>
+						<p>(<span class="blue-font">boolean</span>) Show or hide an overlay that will appear below <code class="language-CSS">#moby</code>, but above the content. Set to <code class="language-javascript">true</code> to show, <code class="language-javascript">false</code> to hide.</p>
+						<p class="default">Default: true</p>
+					</div> <!-- /wrap -->
+					<div class="option">
+						<h2>overlayClass</h2>
+						<p>(<span class="green-font">string</span>) You can add a class to the overlay as well. Target <code class="language-css">#moby-overlay</code> in your CSS to add your own style, or you can use one of the available options.</p>
+						<p>Available options: 'dark' | 'light' | 'your-own-class'</p>
+						<p class="default">Default: 'dark'</p>
+					</div> <!-- /wrap -->
+					<div class="option">
+						<h2>subMenuOpenIcon</h2>
+						<p>(<span class="green-font">string</span>) The icon displaying when sub menus are hidden (to prompt the user to expand it) can be replaced with text, markup, font-awesome, etc.</p>
+						<p class="default">Default: '&lt;span&gt;&amp;#x25BC;&lt;/span&gt;'</p>
+					</div> <!-- /wrap -->
+					<div class="option">
+						<h2>subMenuCloseIcon</h2>
+						<p>(<span class="green-font">string</span>) The icon displaying when the sub menus are showing (to prompt the user to collapse it) can be replaced with text, markup, font-awesome, etc.</p>
+						<p class="default">Default: '&lt;span&gt;&amp;#x25B2;&lt;/span&gt;'</p>
+					</div> <!-- /wrap -->
+					
+					
+				</div> <!-- /container -->
+			</section>
+			
+
+	</article> <!-- /content -->
+	<?php require_once('./inc/footer.php'); ?>
---
+++ examples.php
@@ -0,0 +1,118 @@
+<?php
+	$active = 'examples';
+	$title = 'Examples';
+	require_once('./inc/header.php');
+?>
+	<section id="top-section">
+		<div class="container">
+			<h2>Moby.js Examples</h2>
+			<p>Examples that showcase some of Moby's options</p>
+			<a href="<?php echo BASE_URL;?>/dist/moby1.0.zip">Download 1.0</a>
+		</div> <!-- /container -->
+	</section> <!-- / top-section -->
+	<article class="content">
+			<section class="separator">
+				<div class="container">
+					<h1>Using the Preset Animations</h1>
+					<p>Moby.js comes packaged with 7 different transitions. All of these are editable throught the moby.less or moby.css files. Click each button below to view the different transitions.</p>
+					<p class="center-text buttons-wrapper">
+						<span class="trigger-menu demo-example button no-caps" data-target="left-side" id="left-side">"left-side"</span>
+						<span class="trigger-menu demo-example button no-caps" data-target="right-side" id="right-side">"right-side"</span>
+						<span class="trigger-menu demo-example button no-caps" data-target="top-full" id="top-full">"top-full"</span>
+						<span class="trigger-menu demo-example button no-caps" data-target="bottom-full" id="bottom-full">"bottom-full"</span>
+						<span class="trigger-menu demo-example button no-caps" data-target="grow-out" id="grow-out">"grow-out"</span>
+						<span class="trigger-menu demo-example button no-caps" data-target="shrink-in" id="shrink-in">"shrink-in"</span>
+						<span class="trigger-menu demo-example button no-caps" data-target="fade" id="fade">"fade"</span>
+					</p>
+					<p>To change the animation, specify one of the presets above for the <code class="language-js">menuClass</code> property, OR assign your own class and style it with less / css.</p>
+<pre>
+<code class="language-javascript">
+// The defualt value for menuClass is "right-side"
+$('nav#main-nav').moby({
+	mobyTrigger  :  $('#moby-button'), // Button that will trigger the Moby menu to open
+	menuClass    :  'grow-out' // This is the class that is added to #moby (Mobile menu)
+});</code>
+</pre>
+				</div> <!-- /container -->
+			</section>
+			<section class="separator">
+				<div class="container">
+					<h1>Customizing the Close Button</h1>
+					<p>When you assign Moby.js to a <code class="language-markup">&lt;nav&gt;</code>, you have the option to customize what the close button says (And if you want a close button). The menus in this demo take advantage of Font-Awesome's close icon.</p>
+					<p class="center-text"><span class="trigger-menu button" data-target="right-side">Take a look</span></p>
+					<p>To change the contents of the close button, add the markup you wish to the <code class="language-markup">closeButtonContent</code> property.</p>
+<pre>
+<code class="language-javascript">
+// The defualt value for closeButtonContent is "&lt;span&gt;X&lt;/span&gt; Close Menu"
+$('nav#main-nav').moby({
+	mobyTrigger         :  $('#moby-button'), // Button that will trigger the Moby menu to open
+	closeButton         :  true, // If set to false the cose button will not appear
+	closeButtonContent  :  '&lt;i class="fa fa-close"&gt;&lt;/i&gt; Close Menu' // To edit the css target #moby-close
+});</code>
+</pre>
+
+					<div class="clear-float"></div>
+				</div> <!-- /container -->
+			</section>
+			<section class="separator">
+				<div class="container">
+					<h1>Customizing the drop down icons</h1>
+					<p>When there are nested <code class="language-markup">&lt;ul&gt;</code> elements, Moby.js will allow hide them, and allow the user to expand them. Both the "open" and "close" icons are cutomizable. (I recommend using Font-Awesome icons).</p>
+					<p class="center-text"><span class="trigger-menu button" data-target="right-side">Try it out</span></p>
+					<p>To change the contents of the close and open icons, add the markup you wish to the <code class="language-markup">subMenuOpenIcon</code> and <code class="language-markup">subMenuCloseIcon</code> properties.</p>
+					
+<pre>
+<code class="language-javascript">
+// The defualt value for subMenuOpenIcon is "&#x25BC;"
+// The defualt value for subMenuCloseIcon is "&#x25B2;"
+$('nav#main-nav').moby({
+	mobyTrigger         :  $('#moby-button'), // Button that will trigger the Moby menu to open
+	subMenuOpenIcon     :  '&lt;i class="fa fa-angle-down"&gt;&lt;/i&gt;', // icon that shows when the submenu is hidden
+	subMenuCloseIcon    :  '&lt;i class="fa fa-angle-up"&gt;&lt;/i&gt;', // icon that shows when the submenu is showing
+});</code>
+</pre>
+
+					<div class="clear-float"></div>
+				</div> <!-- /container -->
+			</section>
+			<section class="separator">
+				<div class="container">
+					<h1>Inserting content BEFORE the mobile menu</h1>
+					<p>Moby.js gives you the ability to add what ever content you wish to display above the mobile menu. This is useful for displaying logos, extra information that isn't in your <code class="language-markup">&lt;nav&gt;</code>, or just about anything your little heart desires.</p>
+					<p class="center-text"><span class="trigger-menu button" data-target="right-side">Take a look</span></p>
+					<p>To add content before the mobile menu, add the markup or text you wish to the <code class="language-markup">insertBefore</code> property. The content that is inserted before the mobile menu is wrapped with <code class="language-markup">&lt;div id="moby-before"&gt;&lt;/div&gt;</code>.</p>
+					
+<pre>
+<code class="language-javascript">
+// If insertBefore is not specified, this section will not be inserted
+$('nav#main-nav').moby({
+	mobyTrigger         :  $('#moby-button'), // Button that will trigger the Moby menu to open
+	insertBefore		:  '&lt;img src="../images/moby.svg"&gt;' // Content that is inserted into #moby-before
+});</code>
+</pre>
+
+					<div class="clear-float"></div>
+				</div> <!-- /container -->
+			</section>
+			<section class="separator">
+				<div class="container">
+					<h1>Inserting content AFTER the mobile menu</h1>
+					<p>Moby.js gives you the ability to also add what ever content you wish to display below the mobile menu. It's a great spot for social media icons, copyright info, or a final thought.</p>
+					<p class="center-text"><span class="trigger-menu button" data-target="right-side">Take a look</span></p>
+					<p>To add content after the mobile menu, add the markup or text you wish to the <code class="language-markup">insertAfter</code> property. The content that is inserted after the mobile menu is wrapped with <code class="language-markup">&lt;div id="moby-after"&gt;&lt;/div&gt;</code>.</p>
+					
+<pre>
+<code class="language-javascript">
+// If insertAfter is not specified, this section will not be inserted
+$('nav#main-nav').moby({
+	mobyTrigger         :  $('#moby-button'), // Button that will trigger the Moby menu to open
+	insertBefore		:  'If you find a better library, let minnow' // Content that is inserted into #moby-after
+});</code>
+</pre>
+
+					<div class="clear-float"></div>
+				</div> <!-- /container -->
+			</section>
+
+	</article> <!-- /content -->
+	<?php require_once('./inc/footer.php'); ?>
