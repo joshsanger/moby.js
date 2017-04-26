6d8f09849c15422b093136d68cdf3bdb88a0b649
Josh Sanger <joshsanger88@gmail.com>
2017-04-25 22:24:50 -0400

    Added in styling for orange font and changed documentation.

--- documentation.php
+++ documentation.php
@@ -24,23 +24,20 @@
 					<p>Below are the current options available with their defaults. Each options is explained further down the page.</p>
 <pre>
 <code class="language-javascript">
-// Moby.js will duplicate the #main-nav menu
-$('nav#main-nav').moby({
-	breakpoint			: 	1024, 
-	closeButton		 	: 	true, 
-	closeButtonContent 	: 	'&lt;span&gt;X&lt;/span&gt; Close Menu',
-	enableEscape		: 	true, 
-	insertAfter			: 	'', 
-	insertBefore		: 	'',
-	menuClass		 	: 	'right-side', 
-	mobyTrigger		 	: 	$("#moby-button"), 
-	overlay			 	: 	true, 
-	overlayClass 	 	: 	'dark',
-    multiLevel 	 	    : 	false,
-    previousContent     : 	'&lt;span&gt;&amp;larr; Previous&lt;/span&gt;',
-    overlayClass 	 	: 	'dark',
-    subMenuOpenIcon  	: 	'&lt;span&gt;&amp;#x25BC;&lt;/span&gt;',
-	subMenuCloseIcon 	: 	'&lt;span&gt;&amp;#x25B2;&lt;/span&gt;',
+var mobyMenu = new Moby({
+	breakpoint		 : 1024,
+	enableEscape	 : true,
+    menu             : $('#main-nav'),
+	menuClass		 : 'right-side',
+	mobyTrigger		 : $("#moby-button"),
+    onClose          : false,
+    onOpen           : false,
+	overlay			 : true,
+	overlayClass 	 : 'dark',
+    overlayClass 	 : 'dark',
+    subMenuOpenIcon  : '&lt;span&gt;&amp;#x25BC;&lt;/span&gt;',
+	subMenuCloseIcon : '&lt;span&gt;&amp;#x25B2;&lt;/span&gt;',
+    template         : '&lt;div class="moby-wrap"&gt;&lt;div class="moby-close"&gt;&lt;span class="moby-close-icon"&gt;&lt;/span> Close Menu&lt;/div&gt;&lt;div class="moby-menu"&gt;&lt;/div&gt;&lt;/div&gt;'
 });</code>
 </pre>
 				</div> <!-- /container -->
@@ -50,101 +47,98 @@ $('nav#main-nav').moby({
 					<h1>The Options</h1>
 					<div class="option">
 						<h2>breakpoint</h2>
-						<p>(<span class="dark-red-font">integer</span>) The number that is entered should match the breakpoint of when the targeted menu switches to mobile. If <code class="language-CSS">#moby</code> is open when the screen resizes above the set breakpoint, <code class="language-CSS">#moby</code> will close.</p>	
-						<p class="default">Default: 960</p>
-					</div> <!-- /wrap -->
-					<div class="option">
-						<h2>closeButton</h2>
-						<p>(<span class="blue-font">boolean</span>) Show or hide the close button at the top of <code class="language-CSS">#moby</code>. Set to <code class="language-javascript">false</code> to hide, <code class="language-javascript">true</code> to show.</p>	
-						<p class="default">Default: true</p>
-					</div> <!-- /wrap -->
-					<div class="option">
-						<h2>closeButtonContent</h2>
-						<p>(<span class="green-font">string</span>) You can specify the markup or text that you wish to display inside of the close button div. Target <code class="language-css">#moby-close</code> in your CSS to add your own style.</p>	
-						<p class="default">Default: '&lt;span&gt;X&lt;/span&gt; Close Menu'</p>
-					</div> <!-- /wrap -->
+                        <p>(<span class="dark-red-font">integer</span> | <span class="blue-font">boolean</span>) The number that is entered should match the breakpoint of when the targeted menu switches to mobile. If <code class="language-CSS">Moby</code> is open when the screen resizes above the set breakpoint, <code class="language-CSS">Moby</code> will close. Set to <code class="language-JS">false</code> if no breakpoint is desired.</p>
+						<p class="default">Default: 1024</p>
+					</div> <!-- /option -->
 					<div class="option">
 						<h2>enableEscape</h2>
-						<p>(<span class="blue-font">boolean</span>) Enable or disable closing <code class="language-CSS">#moby</code> with the escape key. Set to <code class="language-javascript">true</code> to enable, <code class="language-javascript">false</code> to disable.</p>	
+						<p>(<span class="blue-font">boolean</span>) Enable or disable closing <code class="language-CSS">Moby</code> with the escape key. Set to <code class="language-javascript">true</code> to enable, <code class="language-javascript">false</code> to disable.</p>
 						<p class="default">Default: true</p>
-					</div> <!-- /wrap -->
-					<div class="option">
-						<h2>insertAfter</h2>
-						<p>(<span class="green-font">string</span>) You can specify markup or text that you wish to display below the mobile navigation. Target <code class="language-css">#moby-after</code> in your CSS to add your own style. If left blank, nothing will be inserted.</p>		
-						<p class="default">Default: ''</p>
-					</div> <!-- /wrap -->
-					<div class="option">
-						<h2>insertBefore</h2>
-						<p>(<span class="green-font">string</span>) You can specify markup or text that you wish to display above the mobile navigation (right underneath the close button). Target <code class="language-css">#moby-before</code> in your CSS to add your own style. If left blank, nothing will be inserted.</p>		
-						<p class="default">Default: ''</p>
-					</div> <!-- /wrap -->
+					</div> <!-- /option -->
+                    <div class="option">
+						<h2>menu</h2>
+						<p>(<span class="pink-font">element</span>) The menu that will be cloned. It is advised to use semantic markup.</p>
+						<p class="default">Default: $('#main-nav')</p>
+					</div> <!-- /option -->
 					<div class="option">
 						<h2>menuClass</h2>
-						<p>(<span class="green-font">string</span>) This is the class that will be added to <code class="language-CSS">#moby</code>. There are preset animation you can use, or create your own with css.</p>
+						<p>(<span class="green-font">string</span>) This is the class that will be added to <code class="language-CSS">Moby</code>. There are preset animation you can use, or create your own with css.</p>
 						<p>Available transitions are: 'left-side' | 'right-side' | 'top-full' | 'bottom-full' | 'shrink-in' | 'grow-out' | 'fade' | 'your-own-class'</p>		
 						<p class="default">Default: 'right-side'</p>
-					</div> <!-- /wrap -->
+					</div> <!-- /option -->
 					<div class="option">
 						<h2>mobyTrigger</h2>
-						<p>(<span class="pink-font">element</span>) You MUST specify which which element will trigger <code class="language-CSS">#moby</code> to open. It is recommended to NOT use an <code class="language-markup">&lt;a&gt;</code> tag for the trigger, but instead use a <code class="language-markup">&lt;span&gt;</code> tag. If not set, <code class="language-markup">#moby-button</code> will be set to the default trigger.</p>		
+						<p>(<span class="pink-font">element</span>) The element that will trigger <code class="language-CSS">Moby</code> to open. It is recommended to NOT use an <code class="language-markup">&lt;a&gt;</code> tag for the trigger, but instead use a <code class="language-markup">&lt;span&gt;</code> tag. If not set, <code class="language-markup">#moby-button</code> will be set to the default trigger.</p>
 						<p class="default">Default: $("#moby-button")</p>
-					</div> <!-- /wrap -->
+					</div> <!-- /option -->
+                    <div class="option">
+						<h2>onClose</h2>
+						<p>(<span class="orange-font">function</span> | <span class="blue-font">boolean</span>) Callback function that is ran when <code class="language-CSS">Moby</code> is closed. By default the callback function is disabled.</p>
+						<p class="default">Default: false</p>
+					</div> <!-- /option -->
+                    <div class="option">
+						<h2>onOpen</h2>
+						<p>(<span class="orange-font">function</span> | <span class="blue-font">boolean</span>) Callback function that is ran when <code class="language-CSS">Moby</code> is opened. By default the callback function is disabled.</p>
+						<p class="default">Default: false</p>
+					</div> <!-- /option -->
 					<div class="option">
 						<h2>overlay</h2>
-						<p>(<span class="blue-font">boolean</span>) Show or hide an overlay that will appear below <code class="language-CSS">#moby</code>, but above the content. Set to <code class="language-javascript">true</code> to show, <code class="language-javascript">false</code> to hide.</p>
+						<p>(<span class="blue-font">boolean</span>) Show or hide an overlay that will appear below <code class="language-CSS">Moby</code>, but above the content. Set to <code class="language-javascript">true</code> to show, <code class="language-javascript">false</code> to hide.</p>
 						<p class="default">Default: true</p>
-					</div> <!-- /wrap -->
+					</div> <!-- /option -->
 					<div class="option">
 						<h2>overlayClass</h2>
-						<p>(<span class="green-font">string</span>) You can add a class to the overlay as well. Target <code class="language-css">#moby-overlay</code> in your CSS to add your own style, or you can use one of the available options.</p>
+						<p>(<span class="green-font">string</span>) You can add a class to the overlay as well. Target <code class="language-css">.moby-overlay</code> in your CSS to add your own style, or you can use one of the available options.</p>
 						<p>Available options: 'dark' | 'light' | 'your-own-class'</p>
 						<p class="default">Default: 'dark'</p>
-					</div> <!-- /wrap -->
+					</div> <!-- /option -->
 					<div class="option">
 						<h2>subMenuOpenIcon</h2>
-						<p>(<span class="green-font">string</span>) The icon displaying when sub menus are hidden (to prompt the user to expand it) can be replaced with text, markup, font-awesome, etc.</p>
+						<p>(<span class="green-font">string</span>) The icon displaying when sub menus are hidden (to prompt the user to expand it) can be replaced with text, markup, material-icons, etc.</p>
 						<p class="default">Default: '&lt;span&gt;&amp;#x25BC;&lt;/span&gt;'</p>
-					</div> <!-- /wrap -->
+					</div> <!-- /option -->
 					<div class="option">
 						<h2>subMenuCloseIcon</h2>
-						<p>(<span class="green-font">string</span>) The icon displaying when the sub menus are showing (to prompt the user to collapse it) can be replaced with text, markup, font-awesome, etc.</p>
+						<p>(<span class="green-font">string</span>) The icon displaying when the sub menus are showing (to prompt the user to collapse it) can be replaced with text, markup, material-icons, etc.</p>
 						<p class="default">Default: '&lt;span&gt;&amp;#x25B2;&lt;/span&gt;'</p>
-					</div> <!-- /wrap -->
-                    <div class="option">
-                        <h2>multiLayer</h2>
-                        <p>(<span class="blue-font">boolean</span>) Set to <code class="language-javascript">true</code> to enable multi-layer navigation with previous button for navigating between layers. When using this option, <code class="language-css">#moby-before</code> and <code class="language-css">#moby-after</code> are disabled.</p>
-                        <p class="default">Default: false</p>
-                    </div> <!-- /wrap -->
-                    <div class="option">
-                        <h2>previousContent</h2>
-                        <p>(<span class="green-font">string</span>) This is the markup that is displayed inside of the previous button. By default, the previous button will not show if the user is on the first level of navigation.</p>
-                        <p class="default">Default: '&lt;span&gt;&amp;larr; Previous&lt;/span&gt;'</p>
-                    </div> <!-- /wrap -->
-					
-					
+					</div> <!-- /option -->
+                    <div class="option no-margin">
+						<h2>template</h2>
+						<p>(<span class="green-font">string</span>) The desired template of <code class="language-css">Moby</code>. You can create what ever mark up you wish, the only requirement is you MUST have a <code class="language-css">.moby-menu</code> class in your template, otherwise an error will be thrown. <code class="language-css">.moby-menu</code> is where the cloned menu will be placed.</p>
+                        <p class="default">Default:</p>
+<pre>
+<code class="language-markup">
+&lt;div class="moby-wrap"&gt;
+    &lt;div class="moby-close"&gt;
+        &lt;span class="moby-close-icon"&gt;&lt;/span> Close Menu
+    &lt;/div&gt;
+    &lt;div class="moby-menu"&gt;&lt;/div&gt; &lt;!-- Must be included --&gt;
+&lt;/div&gt;</code>
+</pre>
+					</div> <!-- /option -->
 				</div> <!-- /container -->
 			</section>
 
 			<section>
 				<div class="container">
 					<h1>The Methods</h1>
-					<p>These methods are only to be used <em>after</em> you have already initiated moby.</p>
+					<p>These methods are only to be used <em>after</em> you have already instantiated Moby.</p>
 					<div class="option">
-						<h2>.mobyClose()</h2>
-						<p>Using mobyClose() will manually close the moby menu. Simply target the same menu as you did when you initated moby.</p>
+						<h2>.closeMoby()</h2>
+						<p>Using closeMoby() will manually close the Moby menu. Simply target the same menu as you did when you instantiated Moby.</p>
 <pre>
 <code class="language-javascript">
 // Manually closes Moby
-$('nav#main-nav').mobyClose();</code>
+mobyMenu.closeMoby();</code>
 </pre>
 					</div> <!-- /option -->
 					<div class="option">
-						<h2>.mobyOpen()</h2>
-						<p>Using mobyOpen() will manually open the moby menu. Simply target the same menu as you did when you initated moby.</p>
+						<h2>.openMoby()</h2>
+						<p>Using openMoby() will manually open the Moby menu. Simply target the same menu as you did when you instantiated Moby.</p>
 <pre>
 <code class="language-javascript">
 // Manually opens Moby
-$('nav#main-nav').mobyOpen();</code>
+mobyMenu.openMoby();</code>
 </pre>
 					</div> <!-- /option -->
 					
