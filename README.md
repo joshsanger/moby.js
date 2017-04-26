a78df3fea5f942830c52283045229b82fdd52be3
Josh S <josh.sanger@geekpower.ca>
2017-04-26 07:25:09 -0400

    Finished refactoring index page and updated readme

--- README.md
+++ README.md
@@ -1,22 +1,40 @@
 # moby.js
-Moby.js is a JS Library that assists in creating mobile menus. [View demo and docs](http://www.joshuasanger.ca/libraries/moby)
+Moby.js is a jQuery Library that assists in creating mobile menus. [View demo and docs](http://www.joshuasanger.ca/libraries/moby)
 ## Basic Implementation
-**1**.   Link CSS file in head of document
+**1**. Adding the files to your project
+
+You can [download](http://www.joshuasanger.ca/libraries/moby) the files, or simply include Moby.js in your project by using <a href="https://bower.io/" target="_blank">Bower</a> install.
 ```HTML
-<link type="text/css" rel="stylesheet" href="./path-to-css/moby.css"/>
+<link type="text/css" rel="stylesheet" href="./path-to-bower-components/moby.js/dist/moby.min.css"/>
+-
+OR
+-
+<link type="text/css" rel="stylesheet" href="./path-to-css/moby.min.css"/>
 ```
-OR 
-Import the less file if you are using LESS (If so, thumbs up!)
+
+If you are using less, you can import Moby into your less file
+
 ```CSS
 /* Include in your main less file */
+@import "./path-to-bower-components/moby.js/dist/moby.less";
+-
+OR
+-
 @import "./path-to-css/moby.less";
 ```
-**2**.   Link to jQuery v1.11.1+ (If not using already) and moby.min.js file
+**2**.   Link to jQuery v1.11.1+ (If not using already) and moby.min.js file. (If using Bower, Moby uses jQuery as a dependency)
 ```HTML
-<!-- Moby.js requires jQuery version 1.11.1 or higher -->
+<script src="./path-to-bower-components/jquery/dist/jquery.min.js"></script>
+<script src="./path-to-bower-components/moby.js/dist/moby.min.js"></script>
+-
+OR
+-
 <script src="./path-to-js/jquery-1.11.1.min.js"></script>
 <script src="./path-to-js/moby.min.js"></script>
 ```
+PRO TIP: Google's Material Icons work as a great asset when using Moby.js. [Download Google's Material Icons](http://material.io/icons/)
+
+
 **3**.   Make sure you have the valid markup ready
 ```HTML
 <!-- Standard sample menu -->
@@ -31,14 +49,14 @@ Import the less file if you are using LESS (If so, thumbs up!)
 
 <!-- Do NOT use an anchor tag for the mobile menu button -->
 <span id="#moby-button">
-	<i class="fa fa-bars"></i>
+    <i class="material-icons">menu</i>
 </span>
 ```
 **4**.   Initiate basic Moby.js
 ```JS
-// Target the menu you would like to be mobile friendly
-$('nav#main-nav').moby({
-	mobyTrigger: $('#moby-button'), // Button that will trigger the Moby menu to open
+var mobyMenu = new Moby({
+    menu       : $('#main-nav'), // The menu that will be cloned
+    mobyTrigger: $('#moby-button'), // Button that will trigger the Moby menu to open
 });
 ```
 
--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 				<div class="container">
 					<h1>How it works</h1>
 					<p>Moby.js easily converts a desktop <code class="language-markup">&lt;nav&gt;</code> into a mobile friendly version. When the mobile button is clicked, the mobile nav will animate in using one of the 7 preset animations. If these animations don't suit your fancy, you can simply add your own class to Moby's menu, and style it as you see fit. Nested <code class="language-markup">&lt;ul&gt;</code> (Drop down items) are hidden and can be expanded by the user, while still having the ability to click on the actual link. No more fake top level navigation links required!</p>
-					<p class="center-text"><span class="trigger-menu main-trigger button">Try it out:</span></p>
+					<p class="center-text"><span class="demo-trigger button">Try it out:</span></p>
 				</div> <!-- /container -->
 			</section>
 			<section class="no-separator" id="getting-started">
@@ -24,29 +24,42 @@
 					<h1>Getting started</h1>
 					<h2>1. Adding the files to your project</h2>
 					<div class="wrap">
-						<p>You can include Moby.js in your project by using <a href="https://bower.io/" target="_blank">Bower</a> install.</p>
+						<p>You can <a href="<?php echo BASE_URL;?>/downloads/moby2.0.4.zip">download</a> the files, or simply include Moby.js in your project by using <a href="https://bower.io/" target="_blank">Bower</a> install.</p>
 <pre>
 <code class="language-markup">
 bower install moby.js</code>
 </pre>
 					</div> <!-- /wrap -->
 					<div class="wrap">
-						<p>Point to which ever files you prefer (less, css, minified versions, etc.)</p>
+						<p>Point to which ever style files you prefer (plain css or minified versions)</p>
 <pre>
-<code class="language-css">
-/* Include in your main less file */
-@import "./path-to-css/moby.less";</code>
+<code class="language-markup">
+&lt;link type="text/css" rel="stylesheet" href="./path-to-bower-components/dist/moby.js/moby.min.css"/&gt;
+OR
+&lt;link type="text/css" rel="stylesheet" href="./path-to-css/moby.min.css"/&gt;</code>
 </pre>
 					</div> <!-- /wrap -->
 					<div class="wrap">
-						<p>PRO TIP: Google's Material Icons work as a great asset when using Moby.js. <a href="http://material.io/icons//" target="_blank">Download Google's Material Icons</a></p>
-						<pre><code class="language-markup">&lt;link type="text/css" rel="stylesheet" href="./path-to-font-awesome/css/font-awesome.min.css"/&gt;</code></pre>
+                        <p>If you are using less, you can import Moby into your less file</p>
+<pre>
+<code class="language-css">
+@import './path-to-bower-components/moby.js/dist/moby.less';
+OR
+@import './path-to-css/moby.less';</code>
+</pre>
+						<p>PRO TIP: Google's Material Icons work as a great asset when using Moby.js. <a href="http://material.io/icons/" target="_blank">Download Google's Material Icons</a></p>
 					</div> <!-- /wrap -->
 					<div class="wrap">
 						<p>Link JS file(s) at the bottom of the document, just before <code class="language-markup">&lt;/body&gt;</code></p>
 <pre>
 <code class="language-markup">
-&lt;!-- Moby.js requires jQuery version 1.11.1 or higher --&gt;
+&lt;!--
+    Moby.js requires jQuery version 1.11.1 or higher.
+    If using Bower, jQuery is a dependency and will be downloaded automatically
+--&gt;
+&lt;script src="./path-to-bower-components/jquery/dist/jquery.min.js"&gt;&lt;/script&gt;
+&lt;script src="./path-to-bower-components/moby.js/dist/moby.min.js"&gt;&lt;/script&gt;
+OR
 &lt;script src="./path-to-js/jquery-1.11.1.min.js"&gt;&lt;/script&gt;
 &lt;script src="./path-to-js/moby.min.js"&gt;&lt;/script&gt;</code>
 </pre>
@@ -86,8 +99,8 @@ bower install moby.js</code>
 
 <pre>
 <code class="language-javascript">
-// Target the menu you would like to be mobile friendly
-$('nav#main-nav').moby({
+var mobyMenu = new Moby({
+    menu       : $('#main-nav'), // The menu that will be cloned
 	mobyTrigger: $('#moby-button'), // Button that will trigger the Moby menu to open
 });</code>
 </pre>
